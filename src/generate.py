42053f30e96c9c1424ce60c6cfdf5f82419fcfbf
Robert Hafner <tedivm@tedivm.com>
2015-03-19 00:01:56 -0700

    Built generator for Hieratic manifest
    Rather than build out the ridiculous amount of boiler text that would
    be needed I did this all by hand. This will be unnecessary when the
    iterator functions of the future parser are out but unfortunately is
    the only way to do this at the moment.

---
+++ src/generate.py
@@ -0,0 +1,52 @@
+import os
+import string
+import subprocess
+import sys
+import tempfile
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+resource_dir = os.path.normpath(current_dir + '/../resources/') + '/'
+
+puppet_types = [line.strip() for line in open(resource_dir + 'typelist.txt')]
+puppet_types.sort()
+
+f = open(resource_dir + 'parameters.tmpl')
+parameter_tmpl = string.Template(f.read())
+f.close()
+
+f = open(resource_dir + 'createblock.tmpl')
+typedef_tmpl = string.Template(f.read())
+f.close()
+
+f = open(resource_dir + 'header.tmpl')
+hieratic_class = f.read()
+f.close()
+
+hieratic_class += """\nclass hieratic (
+  $global_enable = true,
+"""
+
+for puppet_type in puppet_types:
+  hieratic_class += parameter_tmpl.substitute(type=puppet_type)
+
+hieratic_class += ") {\n\n"
+
+for puppet_type in puppet_types:
+  hieratic_class += typedef_tmpl.substitute(type=puppet_type) + "\n"
+
+hieratic_class += "}"
+
+
+# puppet-lint only works on files, so we write it to temp and return the result.
+f = tempfile.NamedTemporaryFile()
+f.write(hieratic_class)
+devnull = open(os.devnull, 'w')
+subprocess.call(['puppet-lint',f.name,'--fix'],
+  stdout=devnull, stderr=devnull
+)
+f.seek(0)
+hieratic_class = f.read()
+f.close()
+
+
+print hieratic_class
